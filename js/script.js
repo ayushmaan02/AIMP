474ce3f5ecd096ab48b4aa8c8f152f315ef51b37
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2021-10-14 01:10:01 +0530

    mentor-section-and-scroll

--- js/script.js
+++ js/script.js
@@ -21,7 +21,19 @@ $(document).ready(function(){
 
         $('.login-form').removeClass('popup');
 
+        $('section').each(function(){
 
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let id = $(this).attr('id')
+            let offset = $(this).offset().top - 200;
+
+            if(top > offset && top < offset +height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active')
+            }
+
+        });
 
     });
 
